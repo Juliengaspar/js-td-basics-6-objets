d065e90bf15aaa919699384d57a28537a441381a
Juliengaspar <julien.gaspar@student.hepl.be>
2025-10-18 11:09:50 +0200

    obj calcule bmi

--- starter/script027-CC4.js
+++ starter/script027-CC4.js
@@ -16,3 +16,40 @@ Rappel: BMI = poids / taille^2 = poids / (taille * taille). (poids en kg and tai
 
 BONNE CHANCE 😀
 */
+
+
+const bmi={
+    nomJohn : 'John',
+    nomMark : 'Mark',
+    poidsJohn : parseInt(prompt('entrez le poids de John en KG')),
+    tailleJohn: parseInt(prompt('entrez la taille de John en M')),
+    poidsMark : parseInt(prompt('entrez le poids de Mark en KG')),
+    tailleMark: parseInt(prompt('entrez la taille de Mark en M')),
+
+
+
+
+
+    calculeBMIJohn(){
+        return this.poidsJohn / (this.tailleJohn * this.tailleJohn);    },
+
+    calculeBMIMark(){
+
+        return this.poidsMark / (this.tailleMark * this.tailleMark);    },
+
+    init(){
+        const bmiJohn = this.calculeBMIJohn();
+        const bmiMark = this.calculeBMIMark();
+        if (bmiJohn > bmiMark) {
+            console.log(`Le BMI de ${this.nomJohn} (${bmiJohn.toFixed(2)}) est supérieur à celui de ${this.nomMark} (${bmiMark.toFixed(2)}).`);
+        } else if (bmiJohn === bmiMark) {
+            console.log(`Le BMI de ${this.nomJohn} et de ${this.nomMark} sont égaux (${bmiJohn.toFixed(2)}).`);
+        } else {
+            console.log(`Le BMI de ${this.nomMark} (${bmiMark.toFixed(2)}) est supérieur à celui de ${this.nomJohn} (${bmiJohn.toFixed(2)}).`);
+        }
+
+    }
+}
+
+bmi.init();
+
